15ebccbc3ca9ab75e67534d86d01e2271b72fb29
Markus Thielen <mathielen@gmail.com>
2019-11-05 09:31:00 +0100

    The actual data is wrapped with a "result" property
    Don't know if it is some BC break they made in the SIB API

--- lib/Api/SMTPApi.php
+++ lib/Api/SMTPApi.php
@@ -150,6 +150,10 @@ class SMTPApi
                 $content = $responseBody->getContents();
                 if ($returnType !== 'string') {
                     $content = json_decode($content);
+                    
+                    if ($content->result) {
+                        $content = $content->result;
+                    }
                 }
             }
 
